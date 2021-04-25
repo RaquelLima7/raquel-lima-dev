fdaff8e6d8138c60516973e733d96c7b969975e0
Raquel Lima <raqueljones182@gmail.com>
2021-04-25 19:53:56 -0300

    portfolio

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll(){
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3)/ 4);
+    target.forEach(function(element){
+        if((windowTop) > element.offsetTop){
+            element.classList.add(animationClass);
+        }else{
+            element.classList.remove(animationClass)
+        }
+        console.log(element.offsetTop);
+    })
+}
+
+window.addEventListener('scroll', function(){
+    animeScroll();
+})
