c65570c4ebaf0640dbc43226aed504ffdff7d92b
Raquel Lima <raqueljones182@gmail.com>
2021-05-02 16:22:11 -0300

    projects

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(".project").isotope({
+  itemSelector: '.element-item',
+  layoutMode: 'fitRow',
+});
+
+$(".project .button-group button").click(function(){
+  $(".project .button-group button").removeClass("active");
+  $(this).addClass("active");
+
+  var selector = $(this).attr("data-filter");
+  $(".project grid").isotope({
+    filter: selector
+  });
+  return false
+});
