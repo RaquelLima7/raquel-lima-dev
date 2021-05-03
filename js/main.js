7860ff52b6013f43079a8b59fe35de833adb7717
Raquel Lima <raqueljones182@gmail.com>
2021-05-03 18:12:38 -0300

    change project area

--- js/main.js
+++ js/main.js
@@ -1,22 +1,16 @@
 // Isotope
-$(".project").isotope({
-  itemSelector: '.element-item',
-  layoutMode: 'fitRows',
-});
+// $(".project").isotope({
+//   itemSelector: '.element-item',
+//   layoutMode: 'fitRows',
+// });
 
-$(".project .button-group button").click(function(){
-  $(".project .button-group button").removeClass("active");
-  $(this).addClass("active");
+// $(".project .button-group button").click(function(){
+//   $(".project .button-group button").removeClass("active");
+//   $(this).addClass("active");
 
-  var selector = $(this).attr("data-filter");
-  $(".project grid").isotope({
-    filter: selector
-  });
-  return false
-});
-
-// $('.grid').isotope({
-//   // options
-//   itemSelector: '.grid-item',
-//   layoutMode: 'fitRows'
+//   var selector = $(this).attr("data-filter");
+//   $(".project grid").isotope({
+//     filter: selector
+//   });
+//   return false
 // });
